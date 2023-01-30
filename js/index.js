097282723a0ddd88b921220c28044bd263332803
bejuco <pierrepuentes@gmail.com>
2023-01-30 16:34:29 -0500

    Estilo v1

--- js/index.js
+++ js/index.js
@@ -1,6 +1,16 @@
 var map = new maplibregl.Map({
     container: 'map',
-    style: './resources/styles/index_map.json', // stylesheet location
-    center: [-72.5, 4.6], // starting position [lng, lat]
-    zoom: 6 // starting zoom
+    style: './resources/styles/index_map_1.json', // stylesheet location
+    center: [-69.2, 5.2], // starting position [lng, lat]
+    zoom: 7 // starting zoom
 });
+
+var popup = new maplibregl.Popup({
+    closeButton: false,
+    closeOnClick: false
+})
+.setLngLat([-69.2, 5.2])
+.setHTML(
+    "<h1>Inventar Colombia</h1><h2>Una mirada desde el Orinoco</h2><a href='#'>LINK</a>"
+    )
+.addTo(map);
