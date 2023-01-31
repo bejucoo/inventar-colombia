57502c1c2220ebe72ae752632d00d5bfb1cbe46b
bejuco <pierrepuentes@gmail.com>
2023-01-31 15:38:34 -0500

    Index popups 310123

--- js/index.js
+++ js/index.js
@@ -1,16 +1,30 @@
 var map = new maplibregl.Map({
     container: 'map',
-    style: './resources/styles/index_map.json', // stylesheet location
-    center: [-69.2, 5.2], // starting position [lng, lat]
-    zoom: 7 // starting zoom
+    style: './resources/styles/index_map.json',
+    center: [-69.2, 5.2],
+    zoom: 7
 });
 
-var popup = new maplibregl.Popup({
+var popup1 = new maplibregl.Popup({
     closeButton: false,
-    closeOnClick: false
+    closeOnClick: false,
+    className: "index_popup main bg_white",
+    maxWidth: "none"
 })
-.setLngLat([-69.2, 5.2])
-.setHTML(
-    "<h1>Inventar Colombia</h1><h2>Una mirada desde el Orinoco</h2><a href='#'>LINK</a>"
+    .setLngLat([-69.2, 5.2])
+    .setHTML(
+        "<h1><i>Inventar Colombia</i></h1><h4><i>Una mirada desde el Orinoco</i></h4>"
     )
-.addTo(map);
+    .addTo(map);
+
+var popup2 = new maplibregl.Popup({
+    closeButton: false,
+    closeOnClick: false,
+    className: "index_popup sub bg_red",
+    maxWidth: "180px"
+})
+    .setLngLat([-66.6, 4.1])
+    .setHTML(
+        "<img src='./resources/images/index_siglo_XIII.png'><h1><i>El Siglo XVIII</h1></i><h2><a href='#'>Ir a sección</a></h2>"
+    )
+    .addTo(map);
