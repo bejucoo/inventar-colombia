fc9c40f04e68ffdedaab4c0680042ac326468d79
bejuco <pierrepuentes@gmail.com>
2023-01-31 21:12:10 -0500

    Index popups 310123

--- js/index.js
+++ js/index.js
@@ -1,30 +1,34 @@
 var map = new maplibregl.Map({
     container: 'map',
-    style: './resources/styles/index_map.json',
+    style: './resources/map_styles/index_map.json',
     center: [-69.2, 5.2],
-    zoom: 7
-});
+    zoom: 7,
+    minZoom: 7
+}); 
 
-var popup1 = new maplibregl.Popup({
-    closeButton: false,
-    closeOnClick: false,
-    className: "index_popup main bg_white",
-    maxWidth: "none"
-})
-    .setLngLat([-69.2, 5.2])
-    .setHTML(
-        "<h1><i>Inventar Colombia</i></h1><h4><i>Una mirada desde el Orinoco</i></h4>"
-    )
-    .addTo(map);
+var xml_popups = new XMLHttpRequest();
 
-var popup2 = new maplibregl.Popup({
-    closeButton: false,
-    closeOnClick: false,
-    className: "index_popup sub bg_red",
-    maxWidth: "180px"
-})
-    .setLngLat([-66.6, 4.1])
-    .setHTML(
-        "<img src='./resources/images/index_siglo_XIII.png'><h1><i>El Siglo XVIII</h1></i><h2><a href='#'>Ir a sección</a></h2>"
-    )
-    .addTo(map);
+xml_popups.onreadystatechange = function() {
+    if (this.readyState === 4 && this.status === 200) {
+        var popups_arr = JSON.parse(this.responseText);
+        addIndexPopups(popups_arr)
+    }
+};
+
+xml_popups.open("GET", "./resources/json/index_popups.json"); 
+xml_popups.send();
+
+function addIndexPopups(json){
+    json.forEach(function(e){
+        new maplibregl.Popup({
+            closeButton: false,
+            closeOnClick: false,
+            anchor: "center",
+            className: e.class,
+            maxWidth: e.maxWidth
+        })
+        .setLngLat(e.lnglat)
+        .setHTML(e.html)
+        .addTo(map);
+    })
+}
