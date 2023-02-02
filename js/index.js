a236f088a60b47e1ab8494a077658280e788652a
bejuco <pierrepuentes@gmail.com>
2023-02-01 20:44:09 -0500

    Index 010223

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@ var map = new maplibregl.Map({
     container: 'map',
     style: './resources/map_styles/index_map.json',
     center: [-69.2, 5.2],
-    zoom: 7,
+    zoom: 7.4,
     minZoom: 7
 }); 
 
@@ -31,4 +31,33 @@ function addIndexPopups(json){
         .setHTML(e.html)
         .addTo(map);
     })
+}
+
+let attached = false;
+let imageContainer = document.querySelector("#image1");
+
+
+function getElmtImage(elmt) {
+  return elmt.querySelector("img");
+}
+
+function followMouse(event) {
+    imageContainer.style.left = event.x + 10 + 'px';
+    imageContainer.style.top = event.y + 10 + 'px';
+}
+
+function showImage(elmt) {
+    const image = getElmtImage(elmt)
+    if (!attached) {
+        attached = true;
+        imageContainer.style.display = "block";
+        document.addEventListener("mousemove", followMouse);
+    }
+}
+
+function hideImage(elmt) {
+    const image = getElmtImage(elmt)
+    attached = false;
+    imageContainer.style.display = "none";
+    document.removeEventListener("mousemove", followMouse);
 }
