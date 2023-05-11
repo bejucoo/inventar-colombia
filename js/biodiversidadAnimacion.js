b976e765d2684d4225d868d21528bfd6f7c92c3e
bejuco <pierrepuentes@gmail.com>
2023-05-11 12:14:21 +0100

    Narrativa 110523

---
+++ js/biodiversidadAnimacion.js
@@ -0,0 +1,10 @@
+console.log(document.getElementById("animBiodiversidad_1"))
+/*anime({
+  targets: '#biodiversidadAnim_1 #serpiente',
+  strokeDashoffset: [anime.setDashoffset, 40],
+  easing: 'easeInOutSine',
+  duration: 1500,
+  delay: function(el, i) { return i * 250 },
+  direction: 'alternate',
+  loop: true
+});*/
