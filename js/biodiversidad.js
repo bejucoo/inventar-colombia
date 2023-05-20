77d44f80153e5625f645ea76dd4c01ae395088cb
bejuco <pierrepuentes@gmail.com>
2023-05-20 11:53:12 +0200

    CSS, JS & Narrativa 200523

--- js/biodiversidad.js
+++ js/biodiversidad.js
@@ -1,32 +1,33 @@
-const biodiversidad_1 = document.getElementById("biodiversidadTexto_1");
+const biodiversidadText_1 = document.getElementById("biodiversidadTexto_1");
+const biodiversidadAnim_1 = document.getElementById("biodiversidadAnim_1");
 
 // Obtener el archivo de los popups.
-async function fetchIntro() {
+async function fetchBiodiversidad() {
 	try {
-		const response = await fetch("./resources/json/narrativa/biodiversidad.json")
+		const response = await fetch("./resources/json/narrativa/biodiversidad/biodiversidadAnim.json")
 		const data = await response.json();
 		return data;
 	} catch(error) {
-		console.error(error); 
+		console.error(error);
 	}
 }
 
 // Ejecutar las funciones cuando se reciba la respuesta.
-fetchIntro().then(data => scrollIntro(data));
+fetchBiodiversidad().then(data => scrollBiodiversidad(data));
 
-
-const scrollIntro = (text) => {
+// Crear la isntancia de scrollama y llamar una función cuando se cambie de step.
+const scrollBiodiversidad = (data) => {
 	const scroller = scrollama();
 	scroller
 	.setup({
 		step: ".biodiversidadStep"
 	})
 	.onStepEnter((response) => {
-		changeIntroContent(text, response);
-		console.log(response);
+		changeContent(data, response);
 	});
 }
 
-const changeIntroContent = (text, step) => {
-	biodiversidad_1.innerHTML = text[step.element.id].content;
+const changeContent = (data, step) => {
+	biodiversidadText_1.innerHTML = data[step.element.id].text;
+	biodiversidadAnim_1.innerHTML = data[step.element.id].img;
 }
